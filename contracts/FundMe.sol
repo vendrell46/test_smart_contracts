08a97ac7606a893604790f95a30889794520b9f3
vendrell46 <vendrell_46@hotmail.com>
2022-12-17 21:05:46 +0100

    implemented withdraw function and onlyOwner modifier

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -9,11 +9,42 @@ contract FundMe {
     uint256 public minimumUsd = 50 * 1e18;
 
     address[] public funders;
-    mapping (address => uint256) public addressToAmountFunded; 
+    mapping (address => uint256) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier  onlyOwner {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
+    }
 
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // reset the array
+        funders = new address[](0);
+
+        // // actually withdraw the funds
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance); 
+        // require(sendSuccess, "Send failed");
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Send failed");
+
     }
 }
